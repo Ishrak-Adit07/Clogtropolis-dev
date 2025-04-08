c06d10e664222879993f4cbdf3541eb817b411ee
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-04-09 02:51:09 +0600

    Profile page

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Pressable } from "react-native";
+import { useRouter } from "expo-router";
+import { DUMMY_USER } from "@/constants/dummy";
 
-const profile = () => {
+const Profile = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center my-auto">
-      <Text className="text-xl text-center text-blue-400">profile</Text>
+    <View className="flex-1 items-center px-6 mt-10">
+      {/* <Header /> */}
+      <Text className="text-3xl font-bold text-red-400 mb-8 mt-6">
+        User Profile
+      </Text>
+
+      {/* User Info */}
+      <View className="bg-slate-300 p-4 rounded-lg w-full text-black text-base gap-2">
+        <Text className="text-xl">
+          <Text className="font-semibold ">Name:</Text> {DUMMY_USER.name}
+        </Text>
+        <Text className="text-red-400 font-bold text-xl">
+          <Text className="font-semibold text-black">Role:</Text>{" "}
+          {DUMMY_USER.role}
+        </Text>
+      </View>
+
+      <View className="bg-dark-200 p-4 rounded-lg w-full mb-8 text-black text-base gap-2">
+        <Text className="font-semibold text-black">Role:</Text> {DUMMY_USER.id}
+      </View>
+
+      {/* Logout Button */}
+      <Pressable
+        className="w-full h-12 bg-red-400 rounded-md flex items-center justify-center"
+        onPress={() => router.push("../login")}
+      >
+        <Text className="text-white text-lg font-bold">Log out</Text>
+      </Pressable>
     </View>
   );
 };
 
-export default profile;
+export default Profile;
