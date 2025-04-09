0a4edb325afb4c9f4189bc831bcf8a023be624cf
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-04-09 19:37:46 +0600

    Store commit: user

---
+++ client/store/useStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface UserState {
+  currentUser: User | null;
+  setCurrentUser: (user: User) => void;
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  currentUser: null,
+  setCurrentUser: (user) => set({ currentUser: user }),
+}));
